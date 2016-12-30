ed7841d0a3751ee8edc447502470b965cf37d7ca
liangxin <liangxin@mapbar.com>
2016-12-30 14:41:26 +0800

    修改编译配置及style，使能运行

--- app/src/main/java/com/gin/hellojni/MainActivity.java
+++ app/src/main/java/com/gin/hellojni/MainActivity.java
@@ -1,14 +1,14 @@
 package com.gin.hellojni;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
     EditText et;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
